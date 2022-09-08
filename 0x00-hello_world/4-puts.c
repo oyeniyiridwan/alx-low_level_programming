a689bf8a2720c3d7fc693bd1464af71f702df8c1
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-08 07:48:27 -0700

    Second commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*betty
- *style
-*comment
-*/
+
+/**
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
