2adf12abef8cc45e0536fd12370ea138fbe0fccf
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-09 06:26:51 -0700

    Third Commit

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i;
+char p;
+for(i='a'; i<='z';i++)
+{
+putchar(i);
+}
+for(p='A'; p<='Z';p++)    
+{
+putchar(p);
+} 
+putchar('\n');
+return (0);
+}
