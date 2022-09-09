029f6e96972608c579917f57175ad724764f801e
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-09 10:19:42 -0700

    Eighth Commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+int b;
+b = i + 1;
+while (b <= 9)
+{
+int c;
+c = b + 1;
+while(c <= 9)
+{
+if (c != 2)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(i + '0');
+putchar(b + '0');
+putchar(c + '0');
+c++;
+}
+b++;
+}
+}
+putchar('\n');
+return (0);
+}
