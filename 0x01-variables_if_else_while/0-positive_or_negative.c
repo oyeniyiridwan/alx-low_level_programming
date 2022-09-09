3afa5dda4d5a8a609aff5e97d6d1c7af54ea167b
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-09 04:57:10 -0700

    Second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,14 +24,14 @@ printf("%d is negative\n", n);
 }
 else if (n > 0)
 {
-printf("%d is positive\n",n);
+printf("%d is positive\n", n);
 }
 else
 {
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }
 /* your code goes there */
-  
+
 return (0);
-  
+
 }
