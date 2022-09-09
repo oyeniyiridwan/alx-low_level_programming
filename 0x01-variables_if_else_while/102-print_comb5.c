ac001bc7d0b44632c97d2061d867f3e1bc4019b1
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-09 13:22:08 -0700

    finally

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,8 +6,8 @@
 */
 int main(void)
 {
-int i, j;  
-int a, b, c, d;  
+int i, j;
+int a, b, c, d;
 for (i = 0; i < 100; i++)
 {
 a = i / 10;
@@ -21,18 +21,16 @@ if (a < c || (a == c && b < d))
 putchar(a + '0');
 putchar(b + '0');
 putchar(32);
-putchar(c + '0');	      
-putchar(d + '0');    
-if (!(a == 9 && b == 8))		
-{		  
-putchar(44);		  
+putchar(c + '0');
+putchar(d + '0');
+if (!(a == 9 && b == 8))
+{
+putchar(44);
 putchar(32);
 }
 }
 }
-}  
-putchar(10);  
+}
+putchar(10);
 return (0);
 }
-
-	   
