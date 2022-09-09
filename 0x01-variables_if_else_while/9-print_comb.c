b643c1335f9089e4c191666c0bb262c2608ab6dd
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-09 08:45:37 -0700

    Sixth Commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,13 @@ int main(void)
 int i;
 for (i = 0; i <= 9; i++)
 {
+putchar(i + '0');
 if (i != 9)
 {
-putchar(i + '0');
 putchar(',');
-}
-else
-{
-putchar(i + '0');
 }
 putchar(' ');
 }
+ putchar('\n');
 return (0);
 }
