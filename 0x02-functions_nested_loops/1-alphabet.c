c6faa66e9f7d951d7176f7c116ce1bf8e913cc7a
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-13 05:49:44 -0700

    First commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - print alphabets in lower case
+*
+* Return: 0 Always (Success)
+*/
+
+void print_alphabet(void)
+{
+char i;
+for (i = "a"; i < "z"; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
