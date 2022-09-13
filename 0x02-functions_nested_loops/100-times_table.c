5122d5368d5b5f2a2fd37dfb6b189ceff97ad780
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-13 12:56:33 -0700

    Commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* times_table - print multiplication table of n
+* @n: parameter
+*
+* Return: 0 Always (success)
+*/
+
+void print_times_table(int n)
+{
+int i;
+i = 0;
+while (i <= n)
+{
+int p;
+
+for (p = 0; p <= n; p++)
+{
+int w;
+w = i *p;
+if (p != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (w  > 9)
+{
+_putchar(w / 10 + '0');
+}
+else if (p != 0)
+{
+_putchar(' ');
+}
+_putchar(w % 10 + '0');
+}
+_putchar('\n');
+i++;
+}
+}
