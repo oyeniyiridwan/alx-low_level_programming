d9dcb36f3c0a72e8b9ca2b36ebe49e75f940917e
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-13 09:22:48 -0700

    Third commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* jack_bauer - print every minute from 00:00 t0 23:59
+*
+* Return: 0 Always (success)
+*/
+
+void jack_bauer(void)
+{
+int i;
+i = 0;
+while (i < 24)
+{
+int p;
+
+for (p = 0; p < 60; p++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(p / 10 + '0');
+_putchar(p % 10 + '0');
+}
+i++;
+}
+}
