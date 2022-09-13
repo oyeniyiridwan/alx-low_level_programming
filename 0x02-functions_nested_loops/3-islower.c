68be5ace59be6690b628137226b69021f5fa1b55
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-13 06:23:09 -0700

    Second commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _islower - check if c is lowercase
+* @c:parameter
+*
+* Return: 0 Always (Success)
+*/
+
+int _islower(int c)
+{
+if (c.islower)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
