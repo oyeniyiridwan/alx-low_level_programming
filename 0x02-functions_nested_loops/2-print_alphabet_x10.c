7e30fb5be5f9d5d074c8ee8fbaebec1de4b31a55
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-13 06:06:23 -0700

    First commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabets in lower case ten times
+*
+* Return: 0 Always (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+int p = 0;
+while (p < 5)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+p++;
+}
+}
