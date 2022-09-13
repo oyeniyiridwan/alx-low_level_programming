79657cb3cb73ef5232b5e629caa4b20b96a8f3d2
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-13 07:16:18 -0700

    Second commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_sign - check if sign of n
+* @n: parameter
+*
+* Return: 0 Always (Success)
+*/
+
+int print_sign(int n)
+{
+if (n = 0)
+{
+_putchar(0/10 + '0');
+}
+else if (n > 0)
+{
+_putchar('+');
+}
+else
+{
+putchar('-');
+}
+return (n > 0 || n < 0);
+}
