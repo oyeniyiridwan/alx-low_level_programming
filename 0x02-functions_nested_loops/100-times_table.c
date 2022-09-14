2586e545fd71c948f342f0fd3efcde06fac9996b
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-13 17:25:39 -0700

    finally

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,44 @@
+#include <stdio.h>
 #include "main.h"
 /**
-* print_times_table - Print the n  times table
+* print_times_table - print multiplication table of n
+* @n: parameter
 *
-* @n: int type number
+* Return: 0 Always (success)
 */
 
 void print_times_table(int n)
 {
-int x = 0, y, z;
-if (n > 15 || n < 0)
-return;
-while (x <= n)
+int i = 0;
+if (n <= 15 || n >= 0)
 {
-for (y = 0; y <= n; y++)
+while (i <= n)
 {
-z = x * y;
-if (z > 99)
+int p = 0;
+for (p = 0; p <= n; p++)
 {
-_putchar(z / 100 + '0');
-_putchar((z / 10 % 10) + '0');
-_putchar(z % 10 + '0');
-}
-else if (z > 9)
-{
-_putchar(' ');
-_putchar(z / 10 + '0');
-_putchar(z % 10 + '0');
-}
-else if (y != 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(z + '0');
-}
-else
-_putchar(z + '0');
-if (y != n)
+int w;
+w = i * p;
+if (p != 0)
 {
 _putchar(',');
 _putchar(' ');
 }
-_putchar('\n');
-x++;
+if (w < 100)
+_putchar(' ');
 }
+if (w >= 100)
+{
+_putchar(w / 100 + '0');
+w = w % 100;
+if (w < 10)
+_putchar(w / 10 + '0');
+}
+if (w  > 9)
+_putchar(w / 10 + '0');
+_putchar(w % 10 + '0');
+}
+_putchar('\n');
+i++;
 }
 }
