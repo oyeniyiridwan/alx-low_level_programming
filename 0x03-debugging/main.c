62aa925b06d5814c24397eb11e09b58fc2c47341
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-14 03:14:54 -0700

    finally

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int a;
+a = largest_number(2.0, 972, 3);
+printf("%d largest", a);
+return (0);
+}
