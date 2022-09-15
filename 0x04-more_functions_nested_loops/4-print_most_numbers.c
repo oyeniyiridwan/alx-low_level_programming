9e1d519a2f0f3a0efd3ef8496f1ad1da29e4f3fa
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-15 09:45:01 -0700

    Second commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,10 @@ void print_most_numbers(void)
 int a = 0;
 while (a <= 9)
 {
-if (a != '2' && a != '4')
+if (a != 2 && a != 4)
 {
 _putchar(a + '0');
 }
 a++;
-}
-_putchar('\n');
+}_putchar('\n');
 }
