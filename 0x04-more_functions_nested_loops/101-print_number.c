4912bb7c5ac73a0f9b345ecbfa1550ebca11f4e9
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-15 15:35:23 -0700

    finally

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+* print_number - print n
+* @n: parameter
+*
+* Return: 0 (Success)
+*/
+
+void print_number(int n)
+{
+int a, b, c, d;
+if (n >= 10000)
+{
+_putchar(n / 10000 + '0');
+a = n % 10000;
+}
+else
+{
+a = n;
+}
+if (a >= 1000)
+{
+_putchar(a / 1000 + '0');
+b = a % 1000;
+}
+else
+{
+b = a;
+}
+if (b >= 100)
+{
+_putchar(b / 100 + '0');
+c = b % 100;
+}
+else
+{
+c = b;
+}
+if (c >= 10)
+{
+_putchar(c / 10 + '0');
+d = c % 10;
+}
+else
+{
+d = c;
+}
+_putchar(d / 10 + '0');
+_putchar(d % 10 + '0');
+}
