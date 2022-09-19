92a215aab23c2fea4830905dc0be8bd893b493f1
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-19 13:37:37 -0700

    Fifth

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -8,10 +8,33 @@
 
 int main(void)
 {
-char s1[98];
-char *ptr;
-ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-printf("%s", s1);  
-printf("%s", ptr);  
+  int nb;
+  
+
+  
+  nb = _atoi("98");
+  
+  printf("%d\n", nb);
+  
+  nb = _atoi("-402");
+  
+  printf("%d\n", nb);
+  
+  nb = _atoi("          ------++++++-----+++++--98");
+  
+  printf("%d\n", nb);
+  
+  nb = _atoi("214748364");
+  
+  printf("%d\n", nb);
+  
+  nb = _atoi("0");
+  
+  printf("%d\n", nb);
+  
+  nb = _atoi("Suite 402");
+  
+  printf("%d\n", nb);
+
 return (0);  
 }
