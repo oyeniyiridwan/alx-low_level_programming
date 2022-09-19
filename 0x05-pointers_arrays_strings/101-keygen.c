5987fd23ebdf71265e463293f8d159b747861ae2
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-19 14:05:15 -0700

    finally

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* main - random and valid password
+*
+* Return: o Always(Success)
+*/
+
+int main(void)
+{
+int random_number;
+int sum = 0;
+srand(time(NULL));  
+while (sum + random_number < 2739)
+{
+random_number = (rand() % (127 - 33 + 1)) + 33;
+printf("%c", random_number);
+sum += random_number;
+}
+while (1)
+{
+random_number = (rand() % (127 - 33 + 1)) + 33;
+if (sum + random_number == 2772
+{
+printf("%c", random_number);
+break;
+} 
+}
+printf("\n");
+return (0);
+}
