d138b084eb0c7c68fa1fd937c566d9b33c8e9b24
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-19 11:08:06 -0700

    Third commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,11 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-if ( i != 0)
+if (i != 0)
 {
 printf(", ");
 }
-printf("%d", *(a + i)); 
+printf("%d", *(a + i));
 }
 printf("\n");
 }
