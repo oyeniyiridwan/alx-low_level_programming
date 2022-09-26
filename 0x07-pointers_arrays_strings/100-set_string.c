5ec247dbb00b18ff405a992426de06a014e4af8a
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-26 07:42:19 -0700

    Second

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* set_string - sets value of pointer s to  to
+* @s: parameter
+* @to: parameter
+*
+* Return: Always 0 (Success)
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
