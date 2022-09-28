f8cee477530b55b74f44ddf36d4e7971992e8d4d
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-09-28 08:06:41 -0700

    Second Commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,37 +9,11 @@
 
 int _sqrt_recursion(int n)
 {
-int i = 0;
-int q = n;
-int z = 1;
 if (n == 0 || n == 1)
 return (n);
 if (n < 0)
 return (-1);
-while (q > 9)
-{
-q = q / 10;
-i++;
-}
-if (i % 2 == 0 && i > 2)
-{
-i = (i / 2) - 1;
-while(i - 1 > 0)
-{
-z = z * 10;
-i--;
-}
-}
-else if (i % 2 != 0 && i > 2)
-{
-i = i / 2;
-while(i > 0)
-{	
-z = z * 10;
-i--;
-}
-}
-return (square(n / z , n));
+return (square(1, n));
 }
 
 
@@ -55,9 +29,9 @@ return (square(n / z , n));
 
 int square(int w, int p)
 {
-if (w == 0)
+if (w * w > p)
 return (-1);
-if ((w * w) != p)
-return (square(w - 1, p));
+if ((w * w) == p)
 return (w);
+return (square(w + 1, p));
 }
