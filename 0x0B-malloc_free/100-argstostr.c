22d2fec668c0303d4fb3376b77e113297b325cd9
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-03 11:18:57 -0700

    Third

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,32 +12,32 @@
 
 char *argstostr(int ac, char **av)
 {
-  int i, u, a = 0, w = 0;
-  char *result;
+int i, u, a = 0, w = 0;
+char *result;
 if (ac == 0 || av == NULL)
 return (NULL);
- for (i = 0; i < ac;  i++)
-   {
-     int j = 0;
-     while (av[i][j] != '\0')
-       {
-	 w++;
-	 j++;
-       }
-   }
- result = malloc((w + ac + 1) * sizeof(char));
- for (u = 0; u < ac; u++)
-   {
-     int p = 0;
-     while(av[u][p] != '\0')
-       {
-	   result[a] = av[u][p];
-	 a++;
-	 p++;
-       }
-     result[a] = '\n';
-     a++;
-   }
- result[a] = '\0';
-   return (result);
+for (i = 0; i < ac;  i++)
+{
+int j = 0;
+while (av[i][j] != '\0')
+{
+w++;
+j++;
+}
+}
+result = malloc((w + ac + 1) * sizeof(char));
+for (u = 0; u < ac; u++)
+{
+int p = 0;
+while (av[u][p] != '\0')
+{
+result[a] = av[u][p];
+a++;
+p++;
+}
+result[a] = '\n';
+a++;
+}
+result[a] = '\0';
+return (result);
 }
