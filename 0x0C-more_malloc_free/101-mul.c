239dea0f3106eba9b2b1c1227fdfac7dfeb7395b
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-06 16:06:52 -0700

    Finally

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,7 +10,7 @@
 */
 int main(int argc, char *argv[])
 {
-unsigned long long mul;
+unsigned long mul;
 int i, j;
 	if (argc != 3)
 	{ printf("Error\n");
@@ -26,6 +26,6 @@ int i, j;
 
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lld\n", mul);
+	printf("%ld\n", mul);
 return (0);
 }
