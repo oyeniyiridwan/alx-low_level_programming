73ba8c53903ce348939ae58ac3751e10a841dade
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-06 14:38:19 -0700

    Finally

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,13 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
+* main - multiplies two positive numbers
+* @argc: n arguments
+* @argv: args
+* Return: int
+*/
 int main(int argc, char *argv[])
 {
 unsigned long mul;
