c1ce3a92b354d9a24675d88d3665c8adf674a12d
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-10 01:48:47 -0700

    First

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print name of file t was compiled from
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Introduction to preprocessing in c
