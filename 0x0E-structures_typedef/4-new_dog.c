d7923acf832cea0dbecc50b2a23ebc476d69a142
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-10 05:08:46 -0700

    Third

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 char *n, *o;
+int i, j;
 dog_t *new_dog = malloc(sizeof(dog_t));
 if (!name || !age || !owner)
 return(NULL);
@@ -23,7 +24,9 @@ if (n == NULL)
 free(new_dog);
 return (NULL);
 }
-n = name;
+for (i = 0; name[i] != '\0'; i++)
+n[i] = name[i];
+n[i] = '\0';
 (*new_dog).name = n;
 o = malloc(sizeof(owner));
 if (o == NULL)
@@ -32,7 +35,9 @@ free((*new_dog).name);
 free(new_dog);
 return (NULL);
 }
-o = owner;
+for (j = 0; owner[j] != '\0'; j++)
+o[j] = owner[j];
+o[j] = '\0';
 (*new_dog).owner = o;
 (*new_dog).age = age;
 return (new_dog);
