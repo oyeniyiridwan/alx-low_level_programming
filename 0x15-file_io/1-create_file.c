4d6b9b9f1cbfef3845eef5912362718a06a0f817
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-31 06:34:24 -0700

    first

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,11 +10,11 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int fd = open(filename, O_CREAT | O_WRONLY, 0600), i;
+int fd = open(filename, O_CREAT | O_WRONLY, 0600), i, p;
 for (i = 0; text_content[i]; i++)
 if (fd == -1 || filename == NULL)
 return (-1);
-int p = write(fd, text_content, i);
+p = write(fd, text_content, i);
 if (p == -1)
 return (-1);
 close(fd);
