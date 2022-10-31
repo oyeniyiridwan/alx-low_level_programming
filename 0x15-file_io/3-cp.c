24be7fa6af24e2b48a49cc1b6956e7cd587fe159
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-31 10:31:27 -0700

    finally

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,8 +10,8 @@
 
 int main(int ac, char **av)
 {
-int fd, fd2, res, p, i;
-char *buff  = malloc(sizeof(char) * 1024);
+int fd, fd2, res, p, j, i;
+char buff[1024];
 if (buff == NULL)
 return (-1);
 if (ac != 3)
@@ -20,31 +20,25 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 fd = open(av[1], O_RDONLY);
-if (fd == -1)
+i = read(fd, buff, 1024);
+if (fd == -1 || i == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
 exit(98);
 }
-i = read(fd, buff, 1024);
-p  = close(fd);
-if (p == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %i", p);
-exit(100);
-}
-fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
+fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 res =  write(fd2, buff, i);
 if (fd2 == -1 || res == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write from file %s", av[2]);
 exit(100);
 }
-p = close(fd2);
-if (p == -1)
+p = close(fd);
+j = close(fd2);
+if (p == -1 || j == -1)
 {
 dprintf(2, "Error: Can't close fd %i", p);
 exit(100);
 }
-free(buff);
 return (0);
 }
