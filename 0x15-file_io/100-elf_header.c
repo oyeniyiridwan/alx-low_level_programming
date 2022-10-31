1cc33cebc8d133a3629398009209512b85522ec1
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-31 10:48:49 -0700

    finally

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - display information containes
+* in the ELF header at the start of an ELF file.
+* @argc: argument count
+* @argv: arguments pointers
+*
+* Return: 0 always
+*/
+
+int main(char argc, int *argv[])
+{
+	return (0);
+}
