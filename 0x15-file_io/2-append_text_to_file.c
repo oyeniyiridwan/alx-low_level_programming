ee688b71560f2282f25f26963e0a3dd9c7c8a0c7
ridwan oyeniyi <oyeniyiridwan5@gmail.com>
2022-10-31 10:12:26 -0700

    finally

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,26 +10,22 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd = open(filename, O_WRONLY, O_APPEND), res, i;
-for (i = 0; text_content[i]; i++)
+int fd, res, i = 0;
 if (filename == NULL)
 return (-1);
+fd = open(filename, O_WRONLY, O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
 {
+while (text_content[i])
+i++;
 res = write(fd, text_content, i);
 if (res == -1)
 {
-close(fd);
 return (-1);
 }
+}
 close(fd);
 return (1);
-}
-else
-{
-close(fd);
-return (1);
-}
 }
